658a714ff8250478032514f15dc20a0e5ec9a723
Landon Henrie <Looney1300@gmail.com>
2018-06-08 12:40:05 -0600

    First Commit. Nice Website.

---
+++ server.js
@@ -0,0 +1,48 @@
+let http = require('http');
+let path = require('path');
+let fs = require('fs');
+//let game = require('./scripts/server/game');
+
+let mimeTypes = {
+        '.js' : 'text/javascript',
+        '.html' : 'text/html',
+        '.css' : 'text/css',
+        '.png' : 'image/png',
+        '.jpg' : 'image/jpeg',
+        '.mp3' : 'audio/mpeg3',
+        '.wav' : 'audio/wav'
+    };
+
+function handleRequest(request, response) {
+    let lookup = (request.url === '/') ? '/index.html' : decodeURI(request.url);
+    let file = lookup.substring(1, lookup.length);
+
+    fs.exists(file, function(exists) {
+        if (exists) {
+            fs.readFile(file, function(err, data) {
+                if (err) {
+                    response.writeHead(500);
+                    response.end('Server Error!');
+                } else {
+                    let headers = {'Content-type': mimeTypes[path.extname(lookup)]};
+                    try {
+                        response.writeHead(200, headers);
+                        response.end(data);
+                    } catch(error) {
+                        console.log(error);
+                    }
+                }
+            });
+        } else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+}
+
+let server = http.createServer(handleRequest);
+
+server.listen(3333, function() {
+    // game.initialize(server);
+    console.log('Server is listening on port 3333');
+});
